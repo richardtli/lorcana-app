f10cd7439fa932283e3ff0f16e121166b6a02752
richardtli <43454832+richardtli@users.noreply.github.com>
2026-04-25 12:24:31 -0500

    added filter by same franchise button

--- src/Components/Filters.tsx
+++ src/Components/Filters.tsx
@@ -1,3 +1,30 @@
-export default function Filters(){
-    return <>
+import { useSearchParams } from "react-router-dom";
+import type { CardType } from "../types/card";
+
+type CardDataProps = {
+  cardData: CardType;
+};
+
+
+export default function Filters({cardData}: CardDataProps){
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const thisFranchise = cardData.franchise
+    function toggleFranchise() {
+        setSearchParams((currentParams) => {
+        const nextParams = new URLSearchParams(currentParams);
+
+        if (nextParams.get("franchise") === thisFranchise) {
+            nextParams.delete("franchise");
+        } else {
+            nextParams.set("franchise", thisFranchise);
+        }
+
+        return nextParams;
+    });
+  }
+    return (<>
+        <button className='filter-button'onClick={toggleFranchise}>From this Franchise</button>
+    </>)
 }
--- src/Pages/CardPage.tsx
+++ src/Pages/CardPage.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState, type JSX } from "react";
-import { data, useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import type { CardType } from "../types/card";
 import { createClient } from "@supabase/supabase-js";
 import CardData from "../Components/CardData";
 import Synergies from "../Components/Synergies";
+import Filters from "../Components/Filters";
 
 const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL!,
@@ -13,6 +14,8 @@ const supabase = createClient(
 export default function CardPage(): JSX.Element | null{
     const { id } = useParams()
     const [cardData, setCardData] = useState<null | CardType>(null)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [relatedCardObjects, setRelatedCardObjects] = useState<CardType[]>([])
 
     async function getCardData(){
         const { data, error } = await supabase
@@ -22,16 +25,38 @@ export default function CardPage(): JSX.Element | null{
             .maybeSingle();
         setCardData(data)
     }
+    async function getRelatedCards(){
+        let query =  supabase.from("lorcana_cards").select("*")
+        searchParams.forEach((value, key) => {
+        query = query.eq(key, value);
+        });
+        const { data, error } = await query;
+
+        if (error) {
+            console.error(error);
+            setRelatedCardObjects([]);
+        } else {
+            setRelatedCardObjects(data ?? []);
+        }
+    }
+    
+    useEffect(()=> {
+        getRelatedCards()
+    },[searchParams])
 
     useEffect(() => {
         getCardData()
     },[id])
 
     if (!cardData) return null;
+    if(!relatedCardObjects) return null
     return(
         <>
             <CardData  cardData={cardData} />
-            <Synergies cardData={cardData}/>
+            <div className="synergies-section">
+                <Filters cardData={cardData}/>
+                <Synergies relatedCardObjects={relatedCardObjects} />
+            </div>
         </>
         
     )
